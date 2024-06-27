92f4aeeaeefe2540a7fe3195b5aebfcd1e6fe5aa
MJ-Dev92 <minjae920802@naver.com>
2024-06-28 05:14:49 +0900

    Update : SearchHeader 검색 버튼 추가

--- src/components/common/SearchHeader.tsx
+++ src/components/common/SearchHeader.tsx
@@ -5,16 +5,16 @@ export default function SearchHeader() {
   const [text, setText] = useState("");
 
   return (
-    <header className="w-[600px] flex p-4 text-2xl  mb-4">
+    <header className="w-[600px] flex mb-4">
       <form className="w-full flex">
         <input
-          className="w-8/12 p-2 outline-none bg-[#EEE] text-[#808080] rounded-[8px] ml-[20px]"
+          className="w-8/12 p-2 outline-none bg-[#EEE] rounded-l-[8px] ml-[40px] border-0"
           type="text"
           placeholder="상품검색"
           value={text}
           onChange={(e) => setText(e.target.value)}
         />
-        <button className="bg-zinc-600 px-4">
+        <button className="bg-[#EEE] rounded-r-[8px] mt-1 px-4 h-[40px] box-border">
           <BsSearch />
         </button>
       </form>
