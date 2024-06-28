024c3266ca9466c978fc1e51a3b47ec20e6dc7f5
thdud <thgml1444@naver.com>
2024-06-28 15:24:56 +0900

    Style : buttomNavBar의 아이콘 클릭 후 스타일 변경되는 코드 간단하게 변경ㅇ - url에 따라 img태그 전체가 렌더링 되는 코드 대신 src만 조건부로 렌더링 되도록 설정

--- src/components/common/BottomNav.tsx
+++ src/components/common/BottomNav.tsx
@@ -10,12 +10,20 @@ import UsedActIcon from "../../assets/icons/nav_usedActive.svg";
 import MyIcon from "../../assets/icons/nav_my.svg";
 import MyActIcon from "../../assets/icons/nav_myActive.svg";
 
-const BottomNav: React.FC = () => {
+const BottomNav = () => {
   const location = useLocation();
   return (
     <nav className="fixed bottom-0 mx-auto w-full max-w-[598px] border-t-2 bg-[#fff]">
       <ul className="flex justify-around p-4">
         <Link to="/" className="text-center cursor-pointer">
+          <img
+            src={location.pathname === "/" ? HomeActIcons : HomeIcon}
+            alt="Home"
+            className="w-6 h-6 mx-auto"
+          />
+          <span className="text-xs">홈</span>
+        </Link>
+        {/* <Link to="/" className="text-center cursor-pointer">
           {location.pathname === "/" ? (
             <>
               <img src={HomeActIcons} alt="Home" className="w-6 h-6 mx-auto" />
@@ -26,8 +34,9 @@ const BottomNav: React.FC = () => {
               <img src={HomeIcon} alt="Home" className="w-6 h-6 mx-auto" />
               <span className="text-xs">홈</span>
             </>
-          )}
-        </Link>
+          )} 
+        </Link> */}
+
         <li className="text-center cursor-pointer">
           <img src={ItemsIcon} alt="Home" className="w-6 h-6 mx-auto" />
           <span className="text-xs">상품</span>
@@ -37,19 +46,33 @@ const BottomNav: React.FC = () => {
           <span className="text-xs">관심목록</span>
         </li>
         <Link to="/usedHome" className="text-center cursor-pointer">
-          {location.pathname === "/usedHome" ? (
-            <>
-              <img src={UsedActIcon} alt="Home" className="w-6 h-6 mx-auto" />
-              <span className="text-xs">중고거래</span>
-            </>
-          ) : (
-            <>
-              <img src={UsedIcon} alt="Home" className="w-6 h-6 mx-auto" />
-              <span className="text-xs">중고거래</span>
-            </>
-          )}
+          <img
+            src={location.pathname === "/usedHome" ? UsedActIcon : UsedIcon}
+            alt="Home"
+            className="w-6 h-6 mx-auto"
+          />
+          <span className="text-xs">중고거래</span>
         </Link>
+        {/* {location.pathname === "/usedHome" ? (
+          <>
+            <img src={UsedActIcon} alt="Home" className="w-6 h-6 mx-auto" />
+            <span className="text-xs">중고거래</span>
+          </>
+        ) : (
+          <>
+            <img src={UsedIcon} alt="Home" className="w-6 h-6 mx-auto" />
+            <span className="text-xs">중고거래</span>
+          </>
+        )} */}
         <Link to="/my" className="text-center cursor-pointer">
+          <img
+            src={location.pathname === "/my" ? MyActIcon : MyIcon}
+            alt="Home"
+            className="w-6 h-6 mx-auto"
+          />
+          <span className="text-xs">마이</span>
+        </Link>
+        {/* <Link to="/my" className="text-center cursor-pointer">
           {location.pathname === "/my" ? (
             <>
               <img src={MyActIcon} alt="Home" className="w-6 h-6 mx-auto" />
@@ -61,7 +84,7 @@ const BottomNav: React.FC = () => {
               <span className="text-xs">마이</span>
             </>
           )}
-        </Link>
+        </Link> */}
       </ul>
     </nav>
   );
