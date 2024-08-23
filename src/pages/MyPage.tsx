605d0f168a5a0c5d19703f4cb8ea368d537c0ccd
thdud <thgml1444@naver.com>
2024-08-23 15:02:30 +0900

    Add : 중고 제품 구매가 실제로 이루어지는 페이지가 됨. 판매자에게 알림을 통해 구매 요청을 알려줌
    - 결제하기 기능 플로우
    - 구매자가 결제하기 버튼 클릭 ->
    - 판매자에게 해당 제품의 구매자의 요청 버튼이 생성, 이때 구매자의 UI에는 판매 수락 기다리고 있다는 버튼 생성
    - 판재마가 제품 판매를 수락 하면 ( 버튼클릭 )
    - 중고제품 구매가 완료 + 알림의 상태가 approved로 바뀌고, 구매 목록이 유저에게 저장, 해당 제품의 수량 업데이트까지
    - 수량 업데이트는 아직임

--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -1,14 +1,22 @@
 import { Link, useNavigate, useParams } from "react-router-dom";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { v4 as uuidv4 } from "uuid";
 
 import { defaultImage, makeArr } from "../types/utils";
-import { loadUserData, logout } from "../api/firebase";
+import {
+  getNotificationsForUser,
+  loadUserData,
+  logout,
+  NotificationDataType,
+  updateOrderStatus,
+} from "../api/firebase";
 import Layout from "../components/myPage/_Layout";
 import MyPageSkeleton from "../components/skeleton/MyPageSkeleton";
 
 const MyPage = () => {
   const { userId } = useParams();
   const navigate = useNavigate();
+  const queryClient = useQueryClient();
 
   const {
     data: user,
@@ -28,7 +36,90 @@ const MyPage = () => {
     }
   };
 
-  console.log(user);
+  const { data: notifications }: { data: NotificationDataType[] } = useQuery({
+    queryKey: ["notifications"],
+    queryFn: () => getNotificationsForUser(userId as string),
+    retry: 3,
+    retryDelay: 1000,
+  });
+  console.log(notifications);
+  const orderStateMutation = useMutation({
+    mutationFn: async ({
+      notification,
+      sellerId,
+    }: {
+      notification: NotificationDataType;
+      sellerId: string;
+    }) => {
+      await updateOrderStatus({
+        notification,
+        sellerId,
+      });
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries(
+        {
+          queryKey: ["notifications"],
+          refetchType: "active",
+          exact: true,
+        },
+        { throwOnError: true, cancelRefetch: true }
+      );
+    },
+  });
+  
+  // const mutateUpdateUsedItemQuantity = useMutation({
+  //   mutationFn: async ({
+  //     itemId,
+  //     quantity,
+  //   }: {
+  //     itemId: string;
+  //     quantity: number;
+  //   }) => {
+  //     await updateUsedItemQuantity({ itemId, quantity });
+  //   },
+  //   onSuccess: () => {
+  //     queryClient.invalidateQueries(
+  //       {
+  //         queryKey: ["usedItems"],
+  //         refetchType: "active",
+  //         exact: true,
+  //       },
+  //       { throwOnError: true, cancelRefetch: true }
+  //     );
+  //   },
+  // });
+  
+  const onClickPurchaseApprove = (notification: NotificationDataType) => {
+    // 구매 요청 승인 ( 상태 업데이트 )
+    orderStateMutation.mutate({
+      notification,
+      sellerId: userId as string,
+    });
+
+    // // 구매정보 firebase에 저장
+    // const usedOrderId = uuidv4();
+    // const usedItemsOrdersInfo: UsedItemsOrdersInfoType = {
+    //   id: usedOrderId,
+    //   seller: data.seller,
+    //   itemId: data.itemId,
+    //   itemName: data.itemName,
+    //   itemImage: data.itemImage,
+    //   price: data.price,
+    //   userId: data.userId,
+    //   quantity: quantity,
+    //   createdAt: new Date().toISOString(),
+    // };
+    // addUsedItemsOrderList({ data: usedItemsOrdersInfo });
+
+    // // 제품 수량 업데이트
+    // mutateUpdateUsedItemQuantity.mutate({
+    //   itemId: data.itemId,
+    //   quantity: data.quantity - quantity,
+    // });
+
+    // // userData -> 해당 제품 seller의 수량 빼기 -> isSale 여부 확인 후 문구추가
+  };
 
   if (isError) {
     return (
@@ -52,7 +143,6 @@ const MyPage = () => {
       </Layout>
     );
   }
-
   if (!user) {
     return <div>유저 정보를 불러올 수 없습니다.</div>;
   }
@@ -61,7 +151,7 @@ const MyPage = () => {
     <Layout title="마이페이지">
       <div className="m-16 p-4">
         {/* 프로필 관리 */}
-        <div className="mb-16 border-b-2">
+        <div className="mb-12 border-b-2">
           <div className="flex items-center mb-8 mx-auto">
             <div className="w-16 h-16 bg-gray-200 rounded-full">
               <img
@@ -83,54 +173,102 @@ const MyPage = () => {
           </button>
           <button
             onClick={onClickLogout}
-            className="w-full h-[45px] py-2 mb-16 bg-gray-400 text-white rounded-md"
+            className="w-full h-[45px] py-2 mb-12 bg-gray-400 text-white rounded-md"
           >
             로그아웃
           </button>
         </div>
 
-        {/* 내 상품 관리 */}
-        <div className="space-y-4">
-          <Link
-            to="orderlist"
-            state={{ user: user }}
-            className="flex justify-between items-center p-4 bg-gray-100 rounded-md cursor-pointer"
-          >
-            <span className="text-lg">주문내역</span>
-            <span className="text-lg font-semibold">
-              {makeArr(user.orders).length}
-            </span>
-          </Link>
-          <Link
-            to="salelist"
-            state={{ user }}
-            className="flex justify-between items-center p-4 bg-gray-100 rounded-md cursor-pointer"
-          >
-            <span className="text-lg">판매목록</span>
-            <span className="text-lg font-semibold">
-              {makeArr(user.sales).length}
-            </span>
-          </Link>
-          <Link
-            to="carts"
-            // state={{ user }}
-            className="flex justify-between items-center p-4 bg-gray-100 rounded-md cursor-pointer"
-          >
-            <span className="text-lg">장바구니</span>
-            <span className="text-lg font-semibold">
-              {makeArr(user.carts).length}
-            </span>
-          </Link>
-          <Link
-            to="messageList"
-            state={{ messages: makeArr(user.messages) }}
-            className="flex justify-between items-center p-4 bg-gray-100 rounded-md cursor-pointer"
-          >
-            <span className="text-lg">내 쪽지함</span>
-            <span className="text-lg font-semibold">
-              {makeArr(user.messages).length}
-            </span>
-          </Link>
+        <div>
+          {/* 내 상품 관리 */}
+          <div className="space-y-4 pb-12 mb-8 border-b-2">
+            <Link
+              to="orderlist"
+              state={{ user: user }}
+              className="flex justify-between items-center p-4 bg-gray-100 rounded-md cursor-pointer"
+            >
+              <span className="text-lg">주문내역</span>
+              <span className="text-lg font-semibold">
+                {makeArr(user.orders).length}
+              </span>
+            </Link>
+            <Link
+              to="salelist"
+              state={{ user }}
+              className="flex justify-between items-center p-4 bg-gray-100 rounded-md cursor-pointer"
+            >
+              <span className="text-lg">판매목록</span>
+              <span className="text-lg font-semibold">
+                {makeArr(user.sales).length}
+              </span>
+            </Link>
+            <Link
+              to="carts"
+              // state={{ user }}
+              className="flex justify-between items-center p-4 bg-gray-100 rounded-md cursor-pointer"
+            >
+              <span className="text-lg">장바구니</span>
+              <span className="text-lg font-semibold">
+                {makeArr(user.carts).length}
+              </span>
+            </Link>
+            <Link
+              to="messageList"
+              state={{ messages: makeArr(user.messages) }}
+              className="flex justify-between items-center p-4 bg-gray-100 rounded-md cursor-pointer"
+            >
+              <span className="text-lg">내 쪽지함</span>
+              <span className="text-lg font-semibold">
+                {makeArr(user.messages).length}
+              </span>
+            </Link>
+          </div>
+
+          {/* 중고제품 알림 관리 */}
+          <div className="my-4">
+            <h3 className=" text-left mb-2 text-xl bold">Notification</h3>
+            {notifications?.map((notification) => {
+              const isSalsesPending = notification.status === "pending";
+              const isApproved = notification.status === "approved";
+              return (
+                <div
+                  key={notification.id}
+                  className={`p-4 mb-2 flex justify-between text-left text-white rounded-md ${
+                    isSalsesPending ? "bg-[#8F5BBD]" : "bg-gray-400"
+                  }`}
+                >
+                  {user.userId == notification.buyerId ? (
+                    // 구매자인 경우
+                    <p className="text-white">
+                      [ {notification.itemName} ]
+                      {isSalsesPending
+                        ? " 판매 수락을 기다리고 있습니다."
+                        : " 구매되었습니다."}
+                    </p>
+                  ) : (
+                    // 판매자인 경우
+                    <div className="w-full text-white flex justify-between items-center">
+                      <p>
+                        [ {notification.itemName} ]
+                        {isSalsesPending
+                          ? " 구매 요청이 있습니다."
+                          : " 판매 완료 되었습니다."}
+                      </p>
+                      {isSalsesPending && (
+                        <button
+                          className="px-4 py-2 bg-white text-[#8F5BBD] rounded"
+                          onClick={() => onClickPurchaseApprove(notification)}
+                        >
+                          OK
+                        </button>
+                      )}
+                    </div>
+                  )}
+                  {isApproved && <button>X</button>}
+                </div>
+              );
+            })}
+          </div>
         </div>
       </div>
     </Layout>
